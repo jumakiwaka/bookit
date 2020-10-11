e9095c2ffbc203475fca89b054c503afdd54ade7
jumakiwaka <alijuma232@gmail.com>
2020-10-11 22:57:58 +0300

    fix env variable undefined

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,6 +3,17 @@ import '@firebase/auth';
 import '@firebase/firestore';
 import 'firebase/analytics';
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} = process.env;
+
 const firebaseConfig = {
   apiKey: REACT_APP_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
@@ -11,7 +22,7 @@ const firebaseConfig = {
   storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
   appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
